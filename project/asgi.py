935229a472f43a501304761e0d04ef34a12d9e30
Job Portal- HireStream <rikeshshrestha9821@gmail.com>
2026-02-25 01:27:09 +0545

    asgi.py & wsgi.py  updated

--- project/asgi.py
+++ project/asgi.py
@@ -11,7 +11,12 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'project.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'project.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+if os.environ.get('RENDER'):  # Render sets this to 'true' for all services
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.deployment_settings')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+# settings_module = 'project.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'project.settings'
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_asgi_application()
--- project/wsgi.py
+++ project/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+if os.environ.get('RENDER'):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.deployment_settings')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
