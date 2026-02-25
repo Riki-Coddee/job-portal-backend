f11b53baf6321dd9410e6c36c3a4c2b6a6d697fd
Job Portal- HireStream <rikeshshrestha9821@gmail.com>
2026-02-25 18:54:24 +0545

    build.sh updated

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate  --settings=project.deployment_settings
 
-if [[ $CREATE_SUPERUSER ]]
-then
-    python manage.py createsuperuser --no-input
-fi
+# if [[ $CREATE_SUPERUSER ]]
+# then
+#     python manage.py createsuperuser --no-input
+# fi
