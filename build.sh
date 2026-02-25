504d745c5ff567b953e6d166ef6a10e1fff34155
Job Portal- HireStream <rikeshshrestha9821@gmail.com>
2026-02-25 12:28:54 +0545

    create our superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate  --settings=project.deployment_settings
+
+if[[$CREATE_SUPERSTAR]];
+then 
+    python manage.py createsuperuser --no-input
+fi
